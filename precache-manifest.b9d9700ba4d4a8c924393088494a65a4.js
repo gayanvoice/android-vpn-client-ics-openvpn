c655fef0253ed27282c70210f711f615f9713f34
gayankuruppu <example@example.com>
2019-09-22 11:11:47 +0530

    Updates

---
+++ precache-manifest.b9d9700ba4d4a8c924393088494a65a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ba9113a61dafba82e8d7fbba0598d0",
+    "url": "/android-vpn-client-ics-openvpn/index.html"
+  },
+  {
+    "revision": "4199ab082171b0f07b74",
+    "url": "/android-vpn-client-ics-openvpn/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "26eeb3b7cbd7cef9a737",
+    "url": "/android-vpn-client-ics-openvpn/static/js/2.c2fbbddc.chunk.js"
+  },
+  {
+    "revision": "4199ab082171b0f07b74",
+    "url": "/android-vpn-client-ics-openvpn/static/js/main.89d360c4.chunk.js"
+  },
+  {
+    "revision": "48411d620fd34f116ddf",
+    "url": "/android-vpn-client-ics-openvpn/static/js/runtime-main.dd05d00c.js"
+  },
+  {
+    "revision": "b8e63ecda002d0150f36e5c972923658",
+    "url": "/android-vpn-client-ics-openvpn/static/media/0-android-vpn-client-app.b8e63ecd.gif"
+  }
+]);
